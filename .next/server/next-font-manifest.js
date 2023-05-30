6d784cef0fd1fc698ef735bf6c067088f253c3fb
RomaAlekhin <raipon@yandex.ru>
2023-05-30 23:35:46 +0200

    remove .next folder

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST={pages:{},app:{'/home/dev/git/react-server-components/node_modules/next/font/google/target.css?{"path":"src/app/layout.tsx","import":"Inter","arguments":[{"subsets":["latin"]}],"variableName":"inter"}':["static/media/2aaf0723e720e8b9-s.p.woff2"]},appUsingSizeAdjust:!0,pagesUsingSizeAdjust:!1};
